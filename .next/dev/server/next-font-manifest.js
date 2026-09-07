1b9ade9821560d5c7f9d94d711cb40ee39c2148b
kazuyaasosms-pixel <kazuya.aso.sms@gmail.com>
2026-09-07 09:43:14 +0900

    Updated trace

--- .next/dev/server/next-font-manifest.js
+++ .next/dev/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/project/app/layout\":[\"static/media/24f6ebe2756575bd-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
